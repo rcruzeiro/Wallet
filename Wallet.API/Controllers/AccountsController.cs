23f162ea0125c375909cf9bf3fa652f62a1a3297
Rafael Cruzeiro <rafael.cruzeiro@gmail.com>
2018-12-13 17:36:01 -0200

    sorting out accounts and customers controllers.

---
+++ Wallet.API/Controllers/AccountsController.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Framework.API.Messages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Wallet.Adapter;
+using Wallet.API.Messages.Accounts;
+using Wallet.DI;
+using Wallet.DTO.Accounts;
+
+namespace Wallet.API.Controllers
+{
+    [Route("[controller]")]
+    public class AccountsController : BaseController
+    {
+        public AccountsController(IConfiguration configuration)
+            : base(configuration)
+        { }
+        /// <summary>
+        /// Gets an account by his ID.
+        /// </summary>
+        /// <returns>The account.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="id">Account identifier.</param>
+        /// <response code="200">Get request returned with sucess.</response>
+        /// <response code="404">Account not found.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(GetAccountResponse), 200)]
+        [ProducesResponseType(typeof(GetAccountResponse), 404)]
+        [ProducesResponseType(typeof(GetAccountResponse), 500)]
+        [HttpGet("{id}")]
+        public ActionResult<GetAccountResponse> Get([FromHeader]string client, [FromRoute]string id)
+        {
+            GetAccountResponse response = new GetAccountResponse();
+            string responseCode = $"GET_ACCOUNT_{client}_{id}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetAccount(_configuration, true);
+                var account = factory.GetAccount(client, id);
+
+                if (account == null)
+                {
+                    response.StatusCode = "404";
+                    response.Messages.Add(ResponseMessage.Create(responseCode, "account not found."));
+                    return NotFound(response);
+                }
+
+                response.StatusCode = "200";
+                response.Data = account.Adapt();
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Creates a new giftcard.
+        /// </summary>
+        /// <returns>The giftcard.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="request">Request DTO.</param>
+        /// <response code="200">Giftcard created with success.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(CreateAccountResponse), 200)]
+        [ProducesResponseType(typeof(CreateAccountResponse), 500)]
+        [HttpPost]
+        public async Task<ActionResult<CreateAccountResponse>> CreateGiftcard([FromHeader]string client, [FromBody]CreateAccountRequest request)
+        {
+            CreateAccountResponse response = new CreateAccountResponse();
+            string responseCode = $"CREATE_GIFT_{client}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetCreateAccount(_configuration);
+                var account = await factory.Create(client, null,
+                                                   request.AccountID, request.LocationID,
+                                                   request.InitialValue, request.ExpiresOn,
+                                                   2, request.ExtensionAttributes);
+                var dto = new NewAccountDTO
+                {
+                    AccountID = account.Item1,
+                    TransactionID = account.Item2
+                };
+                response.StatusCode = "200";
+                response.Data = dto;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Update the account.
+        /// </summary>
+        /// <returns>The updated account ID.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="id">Account identifier.</param>
+        /// <param name="request">Request DTO.</param>
+        /// <response code="200">Account updated with success.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(UpdateAccountResponse), 200)]
+        [ProducesResponseType(typeof(UpdateAccountResponse), 500)]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UpdateAccountResponse>> Put([FromHeader]string client, [FromRoute]string id, [FromBody]UpdateAccountRequest request)
+        {
+            UpdateAccountResponse response = new UpdateAccountResponse();
+            string responseCode = $"UPDATE_{client}_{id}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetAccount(_configuration, true);
+                var account = await factory.UpdateGiftcard(client, id, request.CPF);
+                var dto = new UpdateAccountDTO
+                {
+                    AccountID = account.AccountID
+                };
+                response.StatusCode = "200";
+                response.Data = dto;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Charge an giftcard.
+        /// </summary>
+        /// <returns>The transaction ID.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="id">Account identifier.</param>
+        /// <param name="request">Request DTO.</param>
+        /// <response code="200">The giftcard was charged successfully.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ManageAccountResponse), 200)]
+        [ProducesResponseType(typeof(ManageAccountResponse), 500)]
+        [HttpPost("{id}/charge")]
+        public async Task<ActionResult<ManageAccountResponse>> Charge([FromHeader]string client, [FromRoute]string id, [FromBody]ChargeAccountRequest request)
+        {
+            ManageAccountResponse response = new ManageAccountResponse();
+            string responseCode = $"CHARGE_{client}_{id}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetChargeGiftcard(_configuration);
+                var transaction = await factory.Charge(client, id,
+                                                   request.LocationID, request.Value, request.NowExpiresOn);
+                var dto = new ManageAccountDTO
+                {
+                    TransactionID = transaction
+                };
+                response.StatusCode = "200";
+                response.Data = dto;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Consume the balance of an account.
+        /// </summary>
+        /// <returns>The transaction ID.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="id">Account identifier.</param>
+        /// <param name="request">Request DTO.</param>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ManageAccountResponse), 200)]
+        [ProducesResponseType(typeof(ManageAccountResponse), 500)]
+        [HttpPost("{id}/consume")]
+        public async Task<ActionResult<ManageAccountResponse>> ConsumeAccount([FromHeader]string client, [FromRoute]string id, [FromBody]ConsumeAccountRequest request)
+        {
+            ManageAccountResponse response = new ManageAccountResponse();
+            string responseCode = $"CONSUME_{client}_{id}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetConsumeAccount(_configuration);
+                var transaction = await factory.Consume(client, id, request.LocationID, request.Value);
+                var dto = new ManageAccountDTO
+                {
+                    TransactionID = transaction
+                };
+                response.StatusCode = "200";
+                response.Data = dto;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+    }
+}
---
+++ Wallet.API/Controllers/CustomersController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Framework.API.Messages;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Wallet.Adapter;
+using Wallet.API.Messages.Accounts;
+using Wallet.DI;
+using Wallet.DTO.Accounts;
+
+namespace Wallet.API.Controllers
+{
+    [Route("[controller]")]
+    public class CustomersController : BaseController
+    {
+        public CustomersController(IConfiguration configuration)
+            : base(configuration)
+        { }
+        /// <summary>
+        /// Gets all accounts associated to the CPF.
+        /// </summary>
+        /// <returns>The accounts.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="cpf">Customer CPF.</param>
+        /// <response code="200">Get request returned with success.</response>
+        /// <response code="404">No accounts was found for the specified CPF.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(GetAccountsResponse), 200)]
+        [ProducesResponseType(typeof(GetAccountsResponse), 404)]
+        [ProducesResponseType(typeof(GetAccountsResponse), 500)]
+        [HttpGet("{cpf}")]
+        public ActionResult<GetAccountsResponse> GetAccounts([FromHeader]string client, [FromRoute]string cpf)
+        {
+            GetAccountsResponse response = new GetAccountsResponse();
+            string responseCode = $"GET_ACCOUNTS_{client}_{cpf}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetAccount(_configuration, true);
+                var accounts = factory.GetAccounts(client, cpf);
+
+                if (accounts.Count == 0)
+                {
+                    response.StatusCode = "404";
+                    response.Messages.Add(ResponseMessage.Create(responseCode, "no account found for this client."));
+                    return NotFound(response);
+                }
+
+                response.StatusCode = "200";
+                accounts.ForEach(account =>
+                                 response.Data.Add(account.Adapt()));
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Gets the account type balance.
+        /// </summary>
+        /// <returns>The account balance.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="cpf">Customer CPF.</param>
+        /// <param name="accountType">Account type.</param>
+        /// <response code="200">The account balance returned with success.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(GetAccountBalanceResponse), 200)]
+        [ProducesResponseType(typeof(GetAccountBalanceResponse), 500)]
+        [HttpGet("{cpf}/balance/{accountType}")]
+        public ActionResult<GetAccountBalanceResponse> GetAccountBalance([FromHeader]string client, [FromRoute]string cpf, [FromRoute]int accountType)
+        {
+            GetAccountBalanceResponse response = new GetAccountBalanceResponse();
+            string responseCode = $"GET_BALANCE_{client}_{cpf}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetAccount(_configuration, true);
+                var balance = factory.GetBalance(client, cpf, accountType);
+                response.StatusCode = "200";
+                response.Data = new AccountBalanceDTO
+                {
+                    Balance = string.Format("{0:N}", balance)
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Creates a new voucher.
+        /// </summary>
+        /// <returns>The voucher.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="cpf">Customer CPF.</param>
+        /// <param name="request">Request DTO.</param>
+        /// <response code="200">Voucher created with success.</response>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(CreateAccountResponse), 200)]
+        [ProducesResponseType(typeof(CreateAccountResponse), 500)]
+        [HttpPost("{cpf}")]
+        public async Task<ActionResult<CreateAccountResponse>> CreateVoucher([FromHeader]string client, [FromRoute]string cpf, [FromBody]CreateAccountRequest request)
+        {
+            CreateAccountResponse response = new CreateAccountResponse();
+            string responseCode = $"CREATE_VOUCHER_{client}_{cpf}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetCreateAccount(_configuration);
+                var account = await factory.Create(client, cpf,
+                                                   request.AccountID, request.LocationID,
+                                                   request.InitialValue, request.ExpiresOn,
+                                                   1, request.ExtensionAttributes);
+                var dto = new NewAccountDTO
+                {
+                    AccountID = account.Item1,
+                    TransactionID = account.Item2
+                };
+                response.StatusCode = "200";
+                response.Data = dto;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+        /// <summary>
+        /// Consume the balance of an account type.
+        /// </summary>
+        /// <returns>All transactions IDs.</returns>
+        /// <param name="client">Client identifier.</param>
+        /// <param name="cpf">Customer CPF.</param>
+        /// <param name="accountType">Account Type identifier.</param>
+        /// <param name="request">Request DTO.</param>
+        /// <response code="500">Server internal error. See response messages for details.</response>
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ManageAccountsResponse), 200)]
+        [ProducesResponseType(typeof(ManageAccountsResponse), 500)]
+        [HttpPost("{cpf}/consume/{accountType}")]
+        public async Task<ActionResult<ManageAccountsResponse>> ConsumeBalance([FromHeader]string client, [FromRoute]string cpf, [FromRoute]int accountType, [FromBody]ConsumeAccountRequest request)
+        {
+            ManageAccountsResponse response = new ManageAccountsResponse();
+            string responseCode = $"CONSUME_ACCOUNT_{client}_{accountType}_FROM_{cpf}";
+
+            try
+            {
+                var factory = WalletFactory.Instance.GetConsumeAccount(_configuration);
+                var transactions = await factory.Consume(client, cpf, request.LocationID, accountType, request.Value);
+                response.StatusCode = "200";
+                transactions.ForEach(transaction =>
+                    response.Data.Add(new ManageAccountDTO
+                    {
+                        TransactionID = transaction
+                    }));
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = "500";
+                response.Messages.Add(ResponseMessage.Create(ex, responseCode));
+                return StatusCode(500, response);
+            }
+        }
+    }
+}
